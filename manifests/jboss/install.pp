666dc695e957814089b9b204c799a0f6dfdc5e20
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-26 14:22:48 -0300

    Corrigindo instalação e configuração do JBOSS

--- manifests/jboss/install.pp
+++ manifests/jboss/install.pp
@@ -1,7 +1,9 @@
 class qf_mdsrv_elctrl::jboss::install {
   class { 'wildfly':
-    version        => '8.2.0',
-    install_source => 'http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz',
+    version        => '8.2.1',
+    install_source => 'http://download.jboss.org/wildfly/8.2.1.Final/wildfly-8.2.1.Final.tar.gz',
+    java_home      => '/etc/alternatives/java_sdk',
+    public_http_port  => '7259',
     require        => Package['java-1.8.0-openjdk-devel']
   }
 }
